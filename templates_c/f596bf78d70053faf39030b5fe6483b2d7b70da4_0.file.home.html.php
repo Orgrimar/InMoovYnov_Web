e2d40b0a904dee4d8a9ca6d911ce18c8e56e4b2f
Unknown <rducros34@gmail.com>
2017-12-11 17:19:25 +0100

    V0.1
    Version Init

---
+++ templates_c/f596bf78d70053faf39030b5fe6483b2d7b70da4_0.file.home.html.php
@@ -0,0 +1,26 @@
+<?php
+/* Smarty version 3.1.32-dev-38, created on 2017-12-11 16:19:05
+  from 'C:\wamp64\www\InMoov_Web\InMoovYnov_Web\views\home.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.32-dev-38',
+  'unifunc' => 'content_5a2eaff95d7370_30723546',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'f596bf78d70053faf39030b5fe6483b2d7b70da4' => 
+    array (
+      0 => 'C:\\wamp64\\www\\InMoov_Web\\InMoovYnov_Web\\views\\home.html',
+      1 => 1513009068,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5a2eaff95d7370_30723546 (Smarty_Internal_Template $_smarty_tpl) {
+?>Hello Woardl
+<?php }
+}
